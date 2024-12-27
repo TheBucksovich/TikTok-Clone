e67863b86656057e7b20fb27df3d21f680348abe
Ilya Bochkarev <ilyayurbochkarev@gmail.com>
2024-12-27 19:10:19 +0300

    fix(main): delete read_root and add health.router

--- src/tik_tok/main.py
+++ src/tik_tok/main.py
@@ -1,18 +1,11 @@
 from fastapi import FastAPI
-import uvicorn
-
 from tik_tok.core.config import settings
+from tik_tok.api.routes import health
 
 app = FastAPI(
-    title=settings.project_name,
-    description=settings.project_descr,
-    version=settings.app_version,
+    title=settings.app_name,
     redoc_url=None,
 )
 
-@app.get("/")
-async def read_root():
-    return {"message": f"Welcome to {settings.project_name}!"}
+app.include_router(health.router)
 
-if __name__ == "__main__":
-    uvicorn.run("tik_tok.main:app", host="0.0.0.0", port=3000, reload=True)
