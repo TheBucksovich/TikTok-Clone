5af508dfffd77b988b69fbb838efdd9ba0482590
Ilya Bochkarev <ilyayurbochkarev@gmail.com>
2024-12-27 18:26:35 +0300

    feat(alembic): add env.py with Base metadata and db configuration

---
+++ migrations/env.py
@@ -0,0 +1,64 @@
+import sys
+from pathlib import Path
+import asyncio
+from logging.config import fileConfig
+
+from sqlalchemy import pool
+from sqlalchemy.ext.asyncio import AsyncEngine, create_async_engine
+from alembic import context
+
+from tik_tok.core.config import settings
+from tik_tok.models.base import Base
+import tik_tok.models.users
+
+# Добавление пути до папки src
+sys.path.append(str(Path(__file__).resolve().parents[1] / "src"))
+
+# Alembic configuration
+config = context.config
+if config.config_file_name is not None:
+    fileConfig(config.config_file_name)
+
+target_metadata = Base.metadata
+
+config.set_main_option("sqlalchemy.url", settings.db_dsn)
+
+
+def run_migrations_offline() -> None:
+    """Run migrations in 'offline' mode."""
+    url = config.get_main_option("sqlalchemy.url")
+    context.configure(
+        url=url,
+        target_metadata=target_metadata,
+        literal_binds=True,
+        dialect_opts={"paramstyle": "named"},
+    )
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+def do_run_migrations(connection):
+    context.configure(connection=connection, target_metadata=target_metadata)
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+async def run_migrations_online() -> None:
+    """Run migrations in 'online' mode."""
+    connectable = create_async_engine(
+        settings.db_dsn,
+        poolclass=pool.NullPool,
+    )
+
+    async with connectable.connect() as connection:
+        await connection.run_sync(do_run_migrations)
+
+    await connectable.dispose()
+
+
+if context.is_offline_mode():
+    run_migrations_offline()
+else:
+    asyncio.run(run_migrations_online())
