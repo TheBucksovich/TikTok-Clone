8a59dc8ac793cc1eef01475333fe940a31d0e53a
Ilya Bochkarev <ilyayurbochkarev@gmail.com>
2024-12-27 18:48:23 +0300

    feat(models): create users.py with id, username, email

---
+++ src/tik_tok/models/users.py
@@ -0,0 +1,11 @@
+from sqlalchemy.orm import Mapped, mapped_column
+
+from .base import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column()
+    email: Mapped[str] = mapped_column(unique=True)
