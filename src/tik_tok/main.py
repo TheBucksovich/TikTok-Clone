77409e6ec871b228a928b183402c9af354c1e4e6
Ilya Bochkarev <ilyayurbochkarev@gmail.com>
2024-12-28 11:12:20 +0300

    fix(main): delete health router and add users router witth prefix && tags

--- src/tik_tok/main.py
+++ src/tik_tok/main.py
@@ -1,11 +1,10 @@
 from fastapi import FastAPI
 from tik_tok.core.config import settings
-from tik_tok.api.routes import health
+from tik_tok.api.routes import users
 
 app = FastAPI(
     title=settings.app_name,
     redoc_url=None,
 )
 
-app.include_router(health.router)
-
+app.include_router(users.router, prefix="/users", tags=["Users"])
