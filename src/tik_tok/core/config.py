0c5aca245f117c7b0a8ca00a245e55af5b591287
Ilya Bochkarev <ilyayurbochkarev@gmail.com>
2024-12-28 11:09:48 +0300

    feat(): add secret_key, token, algorithm in Settings

--- src/tik_tok/core/config.py
+++ src/tik_tok/core/config.py
@@ -1,10 +1,10 @@
-from zoneinfo import ZoneInfo
-
 from dynaconf import Dynaconf
 from pydantic import AnyUrl
 from pydantic_settings import BaseSettings
 
-_settings = Dynaconf(settings_files=["config.yaml", ".env"])
+_settings = Dynaconf(
+    settings_files=["config.yaml", ".env"],
+)
 
 _db_dsn = AnyUrl.build(
     scheme="postgresql+asyncpg",
@@ -20,10 +20,18 @@ class Settings(BaseSettings):
     app_name: str
     app_env: str
     db_dsn: str
+    secret_key: str
+    access_token_expire_minutes: int
+    algorithm: str
 
 
 settings = Settings(
     app_name="TikTok Clone",
-    app_env=_settings.app_env,
+    app_env=_settings.get("app_env", "dev"),
     db_dsn=str(_db_dsn),
+    secret_key=_settings.get(
+        "SECRET_KEY", "BMUJVZQZUClKl1RYbD9pcrO-Dcaby0DTjr8FdmJdhxI"
+    ),
+    access_token_expire_minutes=int(_settings.get("ACCESS_TOKEN_EXPIRE_MINUTES", 30)),
+    algorithm=_settings.get("ALGORITHM", "HS256"),
 )
