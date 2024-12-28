11f665d2c40130c9dd1bee4b997fa06ccbae9035
Ilya Bochkarev <ilyayurbochkarev@gmail.com>
2024-12-28 11:14:12 +0300

    feat(models): add a row of hashed_password in User table

--- src/tik_tok/models/users.py
+++ src/tik_tok/models/users.py
@@ -9,3 +9,4 @@ class User(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     username: Mapped[str] = mapped_column()
     email: Mapped[str] = mapped_column(unique=True)
+    hashed_password: Mapped[str] = mapped_column()
