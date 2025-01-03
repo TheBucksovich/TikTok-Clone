133af55df7d6894dfeb8470ff0e4127fa2018cfa
Ilya Bochkarev <ilyayurbochkarev@gmail.com>
2025-01-03 18:58:07 +0300

    feat(logging) add logging configuration

---
+++ src/tik_tok/core/logging.py
@@ -0,0 +1,37 @@
+import logging
+from logging.config import dictConfig
+
+LOGGING_CONFIG = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "default": {
+            "format": "[%(asctime)s] [%(levelname)s] - %(name)s - %(message)s",
+        },
+    },
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+            "formatter": "default",
+        },
+    },
+    "root": {
+        "level": "INFO",
+        "handlers": ["console"],
+    },
+    "loggers": {
+        "uvicorn": {
+            "level": "INFO",
+            "handlers": ["console"],
+            "propagate": False,
+        },
+        "app": {
+            "level": "DEBUG",
+            "handlers": ["console"],
+            "propagate": False,
+        },
+    },
+}
+
+def setup_logging():
+    dictConfig(LOGGING_CONFIG)
