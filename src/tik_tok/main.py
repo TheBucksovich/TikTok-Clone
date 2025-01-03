149d9f5d48388c55a2cce43ffaac7bd0a8e5c772
Ilya Bochkarev <ilyayurbochkarev@gmail.com>
2025-01-03 18:58:31 +0300

    feat(logging) add middleware and video router

--- src/tik_tok/main.py
+++ src/tik_tok/main.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI
 from tik_tok.core.config import settings
 from tik_tok.api.routes import users
+from tik_tok.api.routes import videos
+from tik_tok.core.logging import setup_logging
+from prometheus_fastapi_instrumentator import Instrumentator
+
+from tik_tok.core.middleware import LogRequestMiddleware
+
+setup_logging()
 
 app = FastAPI(
     title=settings.app_name,
@@ -8,3 +15,8 @@ app = FastAPI(
 )
 
 app.include_router(users.router, prefix="/users", tags=["Users"])
+app.include_router(videos.router, prefix="/videos", tags=["Videos"])
+
+
+app.add_middleware(LogRequestMiddleware)
+Instrumentator().instrument(app).expose(app)
