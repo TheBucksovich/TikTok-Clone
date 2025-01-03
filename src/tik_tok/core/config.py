5435b77515d000c8bbdf69b14b69ebac1a1c93c4
Ilya Bochkarev <ilyayurbochkarev@gmail.com>
2025-01-03 18:52:11 +0300

    feat(config.py): add MiniO configuration

--- src/tik_tok/core/config.py
+++ src/tik_tok/core/config.py
@@ -1,10 +1,12 @@
 from dynaconf import Dynaconf
 from pydantic import AnyUrl
 from pydantic_settings import BaseSettings
+from dotenv import load_dotenv
 
 _settings = Dynaconf(
     settings_files=["config.yaml", ".env"],
 )
+load_dotenv(".env")
 
 _db_dsn = AnyUrl.build(
     scheme="postgresql+asyncpg",
@@ -15,7 +17,6 @@ _db_dsn = AnyUrl.build(
     path=_settings.database.db,
 )
 
-
 class Settings(BaseSettings):
     app_name: str
     app_env: str
@@ -23,7 +24,15 @@ class Settings(BaseSettings):
     secret_key: str
     access_token_expire_minutes: int
     algorithm: str
+    s3_endpoint: str
+    s3_access_key: str
+    s3_secret_key: str
+    s3_bucket_name: str
+    rabbitmq_url: str
 
+    class Config:
+        env_file = ".env"
+        extra = "allow"
 
 settings = Settings(
     app_name="TikTok Clone",
@@ -34,4 +43,9 @@ settings = Settings(
     ),
     access_token_expire_minutes=int(_settings.get("ACCESS_TOKEN_EXPIRE_MINUTES", 30)),
     algorithm=_settings.get("ALGORITHM", "HS256"),
+    s3_endpoint=_settings.get("S3_ENDPOINT_URL", "http://0.0.0.0:9001"),
+    s3_access_key=_settings.get("MINIO_ROOT_USER", "minioadmin"),
+    s3_secret_key=_settings.get("MINIO_ROOT_PASSWORD", "minioadmin"),
+    s3_bucket_name=_settings.get("S3_BUCKET_NAME", "videos"),
+    rabbitmq_url=_settings.get("RABBITMQ_URL", "amqp://guest:guest@localhost:5672/"),
 )
